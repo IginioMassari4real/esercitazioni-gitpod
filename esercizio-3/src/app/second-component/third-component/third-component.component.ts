8d2d9d9d63f90baec8d8f0a3eb79fc46ad759813
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-02-01 12:57:29 +0000

    punto finale

--- esercizio-3/src/app/second-component/third-component/third-component.component.ts
+++ esercizio-3/src/app/second-component/third-component/third-component.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-third-component',
   standalone: true,
-  imports: [MatProgressBarModule],
+  imports: [MatProgressBarModule, MatButtonModule],
   templateUrl: './third-component.component.html',
   styleUrl: './third-component.component.css'
 })
 export class ThirdComponentComponent {
-  likes:number = 100;
+  likes:number = 40;
   
  clickBottone(){
   this.likes = this.likes + 1
+  
  }
 }
